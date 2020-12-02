08e652e47ad75a9237425dfd263ac94fe8ac5b15
marcindulewicz <dulewiczm@gmail.com>
2020-12-02 19:25:43 +0100

    Delete some unused usings

--- DiaryUser/Main.cs
+++ DiaryUser/Main.cs
@@ -1,15 +1,10 @@
 ﻿using DiaryUser.Properties;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
+
 
 namespace DiaryUser
 {
